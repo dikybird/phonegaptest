38669cb93531cfeb28d938f42023ac6c0ba52de7
Michael C <m_chippington@hotmail.com>
2014-02-14 17:21:55 +0000

    updated config file

--- js/module1.js
+++ js/module1.js
@@ -1,13 +1,21 @@
 var module1 = (
   function(module){
     module.go = function(result){
-
       try{
-        result.prepend(window.device.uuid+" - <br />");  
+        result.prepend(window.device.model+" <br />");  
       }catch(e){
-        result.prepend(e+" - <br />");
+        result.prepend(e+" <br />");
       }
     }
+    
+    module.go2 = function(result){
+      try{
+        result.prepend(device.model+" <br />");  
+      }catch(e){
+        result.prepend(e+" <br />");
+      }
+    }    
+    
     return module;  
   }
 )(module1 || {});

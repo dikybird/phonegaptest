07de8575b7b1ec06abe66a70e1e1fc20aa767b1b
Michael C <m_chippington@hotmail.com>
2014-02-14 15:12:05 +0000

    test button shows uuid - error corrected

--- js/module1.js
+++ js/module1.js
@@ -1,7 +1,11 @@
 var module1 = (
   function(module){
     module.go = function(result){
-      result.prepend(device.uuid;+" - <br />");  
+      try{
+        result.prepend(device.uuid+" - <br />");  
+      }catch(e){
+        result.prepend(e+" - <br />");
+      }
     }
     return module;  
   }

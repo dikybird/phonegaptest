b3d54fe703268cbf3c9bead44ea8b3282ea2daf5
Michael C <m_chippington@hotmail.com>
2014-02-14 17:15:55 +0000

    updated config file

--- js/module1.js
+++ js/module1.js
@@ -1,11 +1,7 @@
 var module1 = (
   function(module){
     module.go = function(result){
-      try{
-        console.log(window.device);  
-      }catch(e){
-        result.prepend(e+" - <br />");
-      }
+
       try{
         result.prepend(window.device.uuid+" - <br />");  
       }catch(e){

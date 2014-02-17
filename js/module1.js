c28830572e3a867e4156273410be5dfec52c838f
Michael C <m_chippington@hotmail.com>
2014-02-17 15:17:10 +0000

    updated config file

--- js/module1.js
+++ js/module1.js
@@ -2,7 +2,7 @@ var module1 = (
   function(module){
     module.go = function(result){
       try{
-        result.prepend(Date()+" "+ device.model+ + " <br />");  
+        result.prepend(Date()+" "+ " <br />");  
       }catch(e){
         result.prepend(e+" <br />");
       }
